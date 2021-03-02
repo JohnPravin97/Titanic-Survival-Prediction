69181f871f53c778f8397db19cddf8d320d1f9bb
John Pravin <68058163+JohnPravin97@users.noreply.github.com>
2021-03-02 23:06:16 +0530

    Update Titanic Prediction.py

--- Titanic Prediction.py
+++ Titanic Prediction.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 from functools import reduce
 from sklearn.preprocessing import PolynomialFeatures, StandardScaler, OneHotEncoder
 from sklearn.impute import SimpleImputer
